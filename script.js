134b24f28dc788181a9b81e34eb8c524b30557bb
azmanjamlie <azman.jamlie@gmail.com>
2017-03-26 12:12:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function morestatus(){
     //request = new ActiveXObject('Microsoft.XMLHTTP');
    request.onreadystatechange = updateasyncstatus;
 	 alert('hellocv');
-   request.open("PUT", server, true);
+   request.open("GET", server, true);
 	alert('bah');
    request.send(null);
 }
